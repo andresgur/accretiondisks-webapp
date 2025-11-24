e9391c2f50f7477b1bd2090c3240de1011886221
andresgur <andress.gurpide@gmail.com>
2025-11-24 11:55:23 +0100

    Removed old files locations

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const DaysToSeconds = 24 * 3600;
-export const cgs = 2.99792458e+10; // speed of light in cm/s
-export const M_sun = 1.989e33; // Mass of the Sun in g
-export const Gcgs = 6.67 * 10 ** -8; // in cm^3/g/s^2
-export const mp = 1.67262192e-24; // mass of proton in g
-export const sigma_T = 6.65245854533e-25;
-export const k_T = 0.39772639; // in cm^2/g, opacity for ionized hydrogen
